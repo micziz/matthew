cc9822f9e1e439588b3ac7ff831afa963fb51603
micziz <miczicontent@gmail.com>
2022-07-17 15:48:37 +0200

    allow not save and start comments

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+# Imports
 import click
 from src._parser_ import Parser
 from src.interpreter import Interpreter
 from src.lexer import Lexer
 
-
+# Create a click command
 @click.command()
+# Option for the prompt
 @click.option('--operation', prompt='Operation',
               help='Operation To interpret.')
+# whether to save to text
 @click.option('--stext', is_flag=True, help="Save operation to text")
-def operate(operation, stext):
+@click.option('--nosave', is_flag=True, help="Don't save in operations.py")
+def operate(operation, stext, nosave):
     try:
         text = operation
         lexer = Lexer(text)
@@ -24,8 +28,9 @@ def operate(operation, stext):
             click.echo(f"Saved in {filename}")
         else:
             click.echo(value)
-        with open("operations.txt", "at") as f:
-            f.write(f"\n{operation}") 
+        if not nosave:
+            with open("operations.txt", "at") as f:
+                f.write(f"\n{operation}") 
     except Exception as e:
         click.echo(e)
         
