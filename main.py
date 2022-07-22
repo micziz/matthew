70a57954aa099876796b154d87f0aea45d6323e2
micziz <99438936+micziz@users.noreply.github.com>
2022-07-22 22:48:21 +0200

    **feat** Develop into master (#4)
    * Add click to requirements
    
    * Add basic CLI
    
    * Add dependabot
    
    * Add finished cli
    
    * Fix name
    
    * Add the possibilty to save to text
    
    * Save the operations that are written
    
    * allow not save and start comments
    
    * Add comments to main.py
    
    * Updated readme
    
    * Updated readme
    
    * Add some comments
    
    * Add black
    
    * More comments
    
    * Update black.yml
    
    * More comments
    
    * Add aliases
    
    * Add more options
    
    * Add comments
    
    * Add verbose
    
    * Delete black.yml
    
    * Add silent
    
    * Add docker, fix bug in ast
    
    * Add the possibility to save lexer result
    
    * Remove docker
    
    * Try to remove pychace
    
    * Try to remove pychace
    
    * Save operations.txt in diffrent mode if AST
    
    * Save operations.txt in diffrent mode if lexer
    
    * Save operations.txt in diffrent mode if lexer FIX
    
    * Add version and small fix
    
    * Final readme

--- main.py
+++ main.py
@@ -1,18 +1,119 @@
+# This is the main file, containing the CLI, and the functions needed to lex, parse and interpret
+# the operation. All code inside this file, unless otherwise noted, is licensed under the Apache-2.0 License
+
+
+# Imports
+import click
 from src._parser_ import Parser
 from src.interpreter import Interpreter
 from src.lexer import Lexer
+from sys import exit
 
-while True:
+# Create a click command
+@click.command()
+# Option for the prompt
+@click.option("--operation", "-O", prompt="Operation", help="Operation To interpret.")
+# whether to save to text
+@click.option("--stext", "-ST", is_flag=True, help="Save operation to text")
+# whether to save to operations.txt (default yes)
+@click.option("--nosave", "-NS", is_flag=True, help="Don't save in operations.py")
+# whether to generate just the ast
+@click.option("--ast", is_flag=True, help="Generate just the Abstract Syntax Tree")
+# whether to run just the lexer
+@click.option("--jlexer", "-JL", is_flag=True, help="Run just the lexer")
+# whether to be verbose
+@click.option("--verbose", "-V", is_flag=True, help="Be more verbose")
+# whether to be silent
+@click.option("--silent", "-S", is_flag=True, help="Be more silent")
+# Operate function.
+def operate(operation, stext, nosave, ast, jlexer, verbose, silent):
+    _version_ = "0.2.0"
+    click.echo(f"matthew version {_version_}")
+    # Try except for errors
     try:
-        text = input("matthew > ")
-        lexer = Lexer(text)
-        tokens = lexer.generate_tokens()
+        # Operation is what is passed in --operation (if not passed it will be prompted)
+        text = operation
+        if verbose:
+            click.echo("Running the lexer")
+        if jlexer:
+            # Initialize the lexer class, with text passed as parameters
+            lexer = Lexer(text)
+            # Generate all tokens
+            tokens = lexer.generate_tokens()
+            if stext:
+                if verbose:
+                    click.echo("Saving in file")
+                # Open save.txt
+                with open("save.txt", "at") as f:
+                    f.write("\n")
+                    for token in tokens:
+                        f.write(str(f"{token} "))
+            else:
+                click.echo(list(tokens))
+            click.echo("Saving the input to operations.txt")
+            with open("operations.txt", "at") as f:
+                f.write(f"\n{operation} (just lexer)")
+            exit()
+        else:
+            lexer = Lexer(text)
+            # Generate all tokens
+            tokens = lexer.generate_tokens()
+        if verbose:
+            click.echo("Generating AST")
+        # Initialize the parser class, with tokens passed as parameters
         parser = Parser(tokens)
+        # Generate a tree
         tree = parser.parse()
-        if not tree: continue
-        interpreter = Interpreter()
-        value = interpreter.visit(tree)
-        print(value)
+        if not ast:
+            if verbose:
+                click.echo("Interpreting")
+            # Initialize the interpreter class
+            interpreter = Interpreter()
+            # Interpret the tree and send it to value
+            value = interpreter.visit(tree)
+            # Filename, needed for later
+        filename = "save.txt"
+        # If the --stext (save text) option is passed:
+        if stext:
+            if verbose:
+                print("Saving in file")
+            # Open save.txt
+            with open(filename, "at") as f:
+                # Save the result of the operation
+                if not ast:
+                    f.write(f"\n{value}")
+                elif ast:
+                    f.write(f"\n{tree}")
+            if silent:
+                # Print that it has been saved.
+                click.echo(f"Saved in {filename}")
+        # Else, simply print the result
+        else:
+            if not ast:
+                click.echo(value)
+            else:
+                click.echo(tree)
+        # If the option --nosave is not passed
+        if not nosave:
+            click.echo("Saving the input to operations.txt")
+            # Then open operations.txt
+            with open("operations.txt", "at") as f:
+                # and save the just inputted operation
+                if ast:
+                    f.write(f"\n{operation} (ast)")
+                else:
+                    f.write(f"\n{operation}")
+        # Else
+        else:
+            if verbose:
+                # Print that it is not going to save to operations.txt
+                click.echo("Will not save the operation in operations.txt")
+    # Except is for catching errors
     except Exception as e:
-        print(e)
-        
+        # Print error
+        click.echo(e)
+
+
+# Run the main function
+if __name__ == "__main__":
+    operate()
--- readme.md
+++ readme.md
@@ -1,7 +1,3 @@
-# DISCLAMER
-
-The project Is in heavy developement, and may change.
-
 <div align="center">
     <h1>Matthew!</h1>
     <p>A fast, simple math interpreter</p>
@@ -10,36 +6,68 @@ The project Is in heavy developement, and may change.
 
 ## Usage
 
-```
+```bash
 python3 main.py
+```
+
+This will prompt you to put an operation. To avoid this, run this command:
+
+```bash
+python3 main.py --operation='operation'
+```
+
+Substitute 'operation' with an actual operation, eg 5 + 5.
+
+For all the commands available, run:
+
+```bash
+python3 main.py --help
 ```
 
 ## Installation
 
 Clone the repo
 
-```
+```bash
 git clone https://github.com/micziz/matthew.git
 cd matthew
 ```
 
+Install dependencies:
+
+**Windows:**
+
+```pwsh
+pip install -r requirements.txt
+```
+
+**MacOS/Linux:**
+
+```bash
+pip3 install -r requirements.txt
+```
+
 Then run main.py
 
+**Windows:**
+
+```pwsh
+python main.py
 ```
+
+**MacOS/Linux:**
+
+```bash
 python3 main.py
 ```
+
+## Introduction
+
+Matthew is a math interpreter that takes your operations and interpret them. It can even save them to a text file.
 
 ## Features
 
 Supports long and short math operations, with parenthesis.
-
-## TODO
-
-- [x] Basic Math Operations
-- [x] Complete Interpreter
-- [ ] Comments
-- [ ] CLI
-
 
 ## License
 
